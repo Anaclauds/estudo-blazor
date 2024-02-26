aa7f4f0c4f8fbfb8adf597f62b0b99d626d56523
Ana Claudia <a.claudia@estudante.ifro.edu.br>
2024-02-26 19:59:03 -0400

    Adicionando meu projeto ao repositório

---
+++ Models/Pessoa.cs
@@ -0,0 +1,48 @@
+﻿namespace AppExemplo1.Models
+{
+    public class Pessoa
+    {
+        public string Nome { get; set; }
+        public double Peso { get; set; }
+        public double Altura { get; set; }
+
+        public double CalcularIMC()
+        {
+            return  Peso / (Altura* Altura);
+        }
+
+        public string Classificar() // fazer depois 
+        {
+            double imc = CalcularIMC();
+            string classificacao;
+
+            if (imc < 18.5)
+            {
+                classificacao = "abaixo do peso ideal.";
+            }
+            else if (imc < 25)
+            {
+                classificacao = "com peso normal.";
+            }
+            else if (imc < 30)
+            {
+                classificacao = "com sobrepeso.";
+            }
+            else if (imc < 35)
+            {
+                classificacao = "com obesidade grau I.";
+            }
+            else if (imc < 40)
+            {
+                classificacao = "com obesidade grau II.";
+            }
+            else
+            {
+                classificacao = "com obesidade grau III.";
+            }
+
+            return Nome + " você está " + classificacao;
+        }
+    }
+    
+}
